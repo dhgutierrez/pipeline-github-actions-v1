36613d5cde49ba54c42a89097792121aa1c124b9
Daniel Huanca <daniel.huanca@gmail.com>
2024-09-20 22:18:45 -0300

    Add Terraform configuration to create VMs on AWS and Azure

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+  common_tags = {
+    owner      = "danielhuanca"
+    managed-by = "terraform"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_key_pub" {
+  description = "Chave pública para a VM na AWS"
+  type        = string
+}
+
+variable "azure_key_pub" {
+  description = "Chave pública para a VM na Azure"
+  type        = string
+}
