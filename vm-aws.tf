bab5cbf2785ff5db465aac3c5cbb0d8992f87dd4
Daniel Huanca <daniel.huanca@gmail.com>
2024-09-21 17:29:19 -0300

    correção EC2

--- vm-aws.tf
+++ vm-aws.tf
@@ -5,7 +5,7 @@ resource "aws_key_pair" "key" {
 
 resource "aws_instance" "vm" {
   ami                         = "ami-0c5410a9e09852edd"
-  instance_type               = "t2.small"
+  instance_type               = "t3a.micro"
   key_name                    = aws_key_pair.key.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
